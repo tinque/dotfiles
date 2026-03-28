66961c29b5957e149f009d530063c78c4a83ab40
tinque <quentin.georget@gmail.com>
2026-03-28 15:23:50 +0100

    feat(nvim): add keymaps for git sync and cleanup

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function run_in_terminal(command)
+  Snacks.terminal(command, { win = { position = "bottom", height = 0.3 }, env = { TERM = vim.env.TERM } })
+end
+
+vim.keymap.set("n", "<leader>gm", function()
+  run_in_terminal([[zsh -lc 'source ~/.zshrc && gsync main']])
+end, { desc = "Git sync main" })
+
+vim.keymap.set("n", "<leader>gn", function()
+  run_in_terminal([[zsh -lc 'source ~/.zshrc && gsync next']])
+end, { desc = "Git sync next" })
+
+vim.keymap.set("n", "<leader>gD", function()
+  run_in_terminal([[zsh -lc 'source ~/.zshrc && gbgD']])
+end, { desc = "Git delete gone branches" })
