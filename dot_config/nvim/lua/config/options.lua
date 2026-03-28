fbfd9b981f5114738f300163ce09903bf0aece6e
tinque <quentin.georget@gmail.com>
2026-03-28 14:37:01 +0100

    fix(nvim): reduce Neovim health warnings

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 vim.opt.scrolloff = 8
 vim.opt.swapfile = false
 vim.opt.undofile = true
+
+-- Luarocks modules (tiktoken_core for CopilotChat)
+package.cpath = package.cpath .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/lib/lua/5.1/?.so"
+
+-- Disable unused providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
