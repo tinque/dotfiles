94598695320f56cfdaa2f8539f9dad5e727ae25c
tinque <quentin.georget@gmail.com>
2026-03-31 10:04:16 +0200

    fix(nvim): remap diffview keys to avoid LazyVim conflicts

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -4,9 +4,9 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
     keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git Diff View" },
-      { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
-      { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
+      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diff View" },
+      { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
     },
     opts = {
       enhanced_diff_hl = true,
