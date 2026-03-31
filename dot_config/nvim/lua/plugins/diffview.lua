874ab86dbad42ee6cebe8d2eb104fa578e2d287d
tinque <quentin.georget@gmail.com>
2026-03-31 09:59:36 +0200

    feat(nvim): add diffview.nvim for split diff view

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git Diff View" },
+      { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { layout = "diff2_horizontal" },
+        merge_tool = { layout = "diff3_mixed" },
+      },
+    },
+  },
+}
