f4a4affc493ec553958cb32fd89e0affc8c15f34
tinque <quentin.georget@gmail.com>
2026-04-08 16:34:54 +0200

    refactor(nvim): remove duplicate blame keymap from gitsigns

---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      -- Disable blame line; use <leader>gb (snacks.git.blame_line) instead
+      { "<leader>ghb", false },
+    },
+  },
+}
