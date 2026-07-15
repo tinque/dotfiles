a5a27964461fdf6db63445d8d98a1f2d171c6d98
tinque <quentin.georget@gmail.com>
2026-07-15 22:08:50 +0200

    chore(nvim): remove copilot integration

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,8 +1,5 @@
 vim.opt.scrolloff = 8
 
--- Luarocks modules (tiktoken_core for CopilotChat)
-package.cpath = package.cpath .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/lib/lua/5.1/?.so"
-
 -- Disable unused providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
